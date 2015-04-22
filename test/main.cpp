93029bc688af14754c56bb5e0641748736f1d3b0
Haruki_Shimizu <can@ameful.org>
2015-04-22 12:59:56 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 清水春貴 on 2015/04/22.
+//  Copyright (c) 2015年 清水春貴. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
